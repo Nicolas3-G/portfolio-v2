f5f517a2d10c5af3efdabc772b0668012837c49a
Yon Valencion <herokiller32e@gmail.com>
2023-07-20 21:21:48 -0700

    Project overlay updates

--- src/components/ProjectSection/ProjectSection.js
+++ src/components/ProjectSection/ProjectSection.js
@@ -1,10 +1,31 @@
+"use client";
 import styles from "./ProjectSection.module.css";
+import { useState } from "react";
 
 
 const ProjectCard = () => {
+    const [displayOverlay, setDisplayOverlay] = useState(false);
+
     return (
-        <div className={styles["project-card"]}>
-            <img src="project-images/crypto-secure.png" className={styles["project-image"]}/>
+        <div onMouseEnter={() => setDisplayOverlay(true)} onMouseLeave={() => setDisplayOverlay(false)} className={styles["project-card"]}>
+            {displayOverlay && <div className={styles.overlay}>
+                <div className={styles["project-top-row"]}>
+                    <div>
+                       <h3 className={styles["project-title"]}>Crypto Secure</h3>
+                       <p className={styles["project-tagline"]}>A cryptocurrency portfolio tracker that allows users to track their crypto holdings and view their portfolio's performance over time.</p> 
+                    </div>
+                    <img src="project-images/crypto-gif.gif" className={styles["mini-image"]} />
+                </div>
+                <div className={styles["project-bottom-row"]}>
+                    <div>
+                    <h4 className={styles["project-subtitle"]}>Technologies Used</h4>
+                    </div>
+                    <button className={styles["site-button"]}>Live Site</button>
+                </div>
+
+                {/* <p className={styles["project-description"]}>A cryptocurrency portfolio tracker that allows users to track their crypto holdings and view their portfolio's performance over time.</p> */}
+            </div>}
+            <img src="project-images/crypto-secure.png" className={styles["project-image"]} />
         </div>
     )
 }
